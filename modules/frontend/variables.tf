e0d5db645816013371d9730d13de2d304312ee85
MagenX <magenx@users.noreply.github.com>
2022-05-25 12:01:35 +0100

    Update variables.tf

--- modules/frontend/variables.tf
+++ modules/frontend/variables.tf
@@ -21,11 +21,6 @@ variable "packer" {
   type         = map
 }
 
-variable "environment" {
-  description  = "Environment selected to deploy this configuration to"
-  type         = string
-}
-
 variable "region" {
   description  = "Region for this project and environment"
   type         = string
