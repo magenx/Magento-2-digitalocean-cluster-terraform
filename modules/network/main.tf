6cc3cb55a23129dcf2da2f593ee551fce1aae100
MagenX <magenx@users.noreply.github.com>
2022-05-25 11:05:04 +0100

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -7,7 +7,7 @@
 # Create our dedicated VPC
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "digitalocean_vpc" "this" {
-  name     = "${var.project_name}-vpc"
+  name     = "${var.project.name}-vpc"
   region   = var.region
   ip_range = var.vpc_cidr
 }
