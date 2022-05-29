c0d755a1fbbbf0da5b73d5c48cead63692331cac
MagenX <magenx@users.noreply.github.com>
2022-05-29 13:41:18 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,15 +1,11 @@
 
 locals {
-  
-  cidr_regex      = regex("^[0-9]+.[0-9]+.", var.vpc_cidr)
-  
-  ssh_config_host = "${local.cidr_regex}.*.*"
-  ssh_port        = random_integer.ssh_port.result
-  
+
   manager_ssh_key = data.digitalocean_ssh_keys.manager.ssh_keys[0]
   admin_ssh_key   = data.digitalocean_ssh_keys.admin.ssh_keys[0]
   
   packer_snapshot = "packer-builder-frontend"
   
   domain          = var.domains[local.environment]
+  
 }
