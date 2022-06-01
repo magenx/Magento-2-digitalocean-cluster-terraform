9d507b5284b53550a8745a53dfebe26114902854
MagenX <magenx@users.noreply.github.com>
2022-06-01 15:00:10 +0100

    Update firewall.tf

--- modules/services/firewall.tf
+++ modules/services/firewall.tf
@@ -26,6 +26,12 @@ resource "digitalocean_firewall" "services" {
   
   outbound_rule {
     protocol         = "tcp"
+    port_range       = "80"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  
+  outbound_rule {
+    protocol         = "tcp"
     port_range       = "443"
     destination_addresses = ["0.0.0.0/0"]
   }
